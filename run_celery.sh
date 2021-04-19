d063dcd34d1522ef35a0763993d060a398488ff8
riggi <simone.riggi@gmail.com>
2021-04-19 10:03:15 +0200

    Add nexcloud volume mount

--- run_celery.sh
+++ run_celery.sh
@@ -11,6 +11,11 @@ RESULT_BACKEND_URL="mongodb://127.0.0.1:27017/caesardb"
 LOG_FILE="/opt/caesar-rest/logs/%n%I.log"
 PID_FILE="/opt/caesar-rest/run/%n.pid"
 QUEUE="celery"
+MOUNT_RCLONE_VOLUME=0
+MOUNT_VOLUME_PATH="/mnt/storage"
+RCLONE_REMOTE_STORAGE="neanias-nextcloud"
+RCLONE_REMOTE_STORAGE_PATH="."
+RCLONE_MOUNT_WAIT_TIME=10
 
 echo "ARGS: $@"
 
@@ -35,7 +40,21 @@ do
 		--queue=*)
     	QUEUE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
     ;;
-		
+		--mount-rclone-volume=*)
+    	MOUNT_RCLONE_VOLUME=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+		--mount-volume-path=*)
+    	MOUNT_VOLUME_PATH=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+		--rclone-remote-storage=*)
+    	RCLONE_REMOTE_STORAGE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+		--rclone-remote-storage-path=*)
+    	RCLONE_REMOTE_STORAGE_PATH=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+		--rclone-mount-wait=*)
+    	RCLONE_MOUNT_WAIT_TIME=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
 
 	*)
     # Unknown option
@@ -52,6 +71,55 @@ echo "Sourcing env vars ..."
 source /etc/profile.d/setupSoft.sh
 echo "PATH=$PATH"
 echo "PYTHONPATH=$PYTHONPATH"
+
+
+###############################
+##    MOUNT VOLUMES
+###############################
+if [ "$MOUNT_RCLONE_VOLUME" = "1" ] ; then
+
+	# - Create mount directory if not existing
+	echo "INFO: Creating mount directory $MOUNT_VOLUME_PATH ..."
+	mkdir -p $MOUNT_VOLUME_PATH	
+
+	# - Get device ID of standard dir, for example $HOME
+	#   To be compared with mount point to check if mount is ready
+	DEVICE_ID=`stat "$HOME" -c %d`
+	echo "INFO: Standard device id @ $HOME: $DEVICE_ID"
+
+	# - Mount rclone volume in background
+	uid=`id -u $RUNUSER`
+
+	echo "INFO: Mounting rclone volume at path $MOUNT_VOLUME_PATH for uid/gid=$uid ..."
+	MOUNT_CMD="/usr/bin/rclone mount --daemon --uid=$uid --gid=$uid --umask 000 --allow-other --dir-cache-time 0m5s --vfs-cache-mode full $RCLONE_REMOTE_STORAGE:$RCLONE_REMOTE_STORAGE_PATH $MOUNT_VOLUME_PATH -vvv"
+	#MOUNT_CMD="/usr/bin/rclone mount --daemon --umask 000 --dir-cache-time 0m5s --vfs-cache-mode full $RCLONE_REMOTE_STORAGE:$RCLONE_REMOTE_STORAGE_PATH $MOUNT_VOLUME_PATH -vvv"
+	#MOUNT_CMD="/usr/bin/rclone mount --daemon --uid=$uid --gid=$uid --umask 000 --allow-other --dir-cache-time 0m5s --vfs-cache-mode full $RCLONE_REMOTE_STORAGE:$RCLONE_REMOTE_STORAGE_PATH $MOUNT_VOLUME_PATH -vvv"
+	eval $MOUNT_CMD
+
+	# - Wait until filesystem is ready
+	echo "INFO: Sleeping $RCLONE_MOUNT_WAIT_TIME seconds and then check if mount is ready..."
+	sleep $RCLONE_MOUNT_WAIT_TIME
+	#until findmnt --mountpoint "$MOUNT_VOLUME_PATH" --mtab >/dev/null; do :; done
+	#findmnt --poll --timeout 2000
+
+	# - Get device ID of mount point
+	MOUNT_DEVICE_ID=`stat "$MOUNT_VOLUME_PATH" -c %d`
+	echo "INFO: MOUNT_DEVICE_ID=$MOUNT_DEVICE_ID"
+	if [ "$MOUNT_DEVICE_ID" = "$DEVICE_ID" ] ; then
+ 		echo "ERROR: Failed to mount rclone storage at $MOUNT_VOLUME_PATH within $RCLONE_MOUNT_WAIT_TIME seconds, exit!"
+		exit 1
+	fi
+
+	# - Print mount dir content
+	echo "INFO: Mounted rclone storage at $MOUNT_VOLUME_PATH with success (MOUNT_DEVICE_ID: $MOUNT_DEVICE_ID)..."
+	ls -ltr $MOUNT_VOLUME_PATH
+
+	# - Create job & data directories
+	echo "INFO: Creating job & data directories ..."
+	mkdir -p 	$MOUNT_VOLUME_PATH/jobs
+	mkdir -p 	$MOUNT_VOLUME_PATH/data
+
+fi
 
 ###############################
 ##    RUN CELERY
